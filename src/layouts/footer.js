efefb0b5a93d54f71e8a001bf64b7db64e03a485
arun <arunc.loma@gmail.com>
2023-04-07 11:20:53 +0700

    wip

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -40,10 +40,10 @@ const Footer = () => {
                 onChange={(event, newValue) => {
                     setValue(newValue);
                 }}>
-                <BottomNavigationAction onClick={() => router.push('/home')} label={t('home')} icon={<HomeIcon color={value == 0?'#FF0000':''} />} />
-                <BottomNavigationAction onClick={() => router.push('/lottery')} label={t('lottery')} icon={<LotterIcon  color={value == 1?'#FF0000':''} />} />
-                <BottomNavigationAction onClick={() => router.push('/match')} label={t('match')} icon={<MatchIcon color={value == 2?'#FF0000':''}/>} />
-                <BottomNavigationAction onClick={() => router.push('/profile')} label={t('profile')} icon={<ProfileIcon color={value == 3?'#FF0000':''} />} />
+                <BottomNavigationAction onClick={() => router.push('/home')} label={t('home')} icon={<HomeIcon color={value == 0 ? '#FF0000':'#8C8C8C'} />} />
+                <BottomNavigationAction onClick={() => router.push('/lottery')} label={t('lottery')} icon={<LotterIcon  color={value == 1?'#FF0000':'#8C8C8C'} />} />
+                <BottomNavigationAction onClick={() => router.push('/match')} label={t('match')} icon={<MatchIcon color={value == 2?'#FF0000':'#8C8C8C'}/>} />
+                <BottomNavigationAction onClick={() => router.push('/profile')} label={t('profile')} icon={<ProfileIcon color={value == 3?'#FF0000':'#8C8C8C'} />} />
             </BottomNavigation>
         </Paper>
 };
