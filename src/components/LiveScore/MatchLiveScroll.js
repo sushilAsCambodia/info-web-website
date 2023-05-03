646ac99d32e3c548da6e5ed4607af40da9e9adc3
vo1an <ngetvirakvoan@gmail.com>
2023-05-03 16:50:45 +0700

    live score page 50% complete

---
+++ src/components/LiveScore/MatchLiveScroll.js
@@ -0,0 +1,71 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography, Divider, Button, Link } from "@mui/material";
+import { Grid, Card, CardHeader } from "@mui/material";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { getNewsByCategory } from "@/store/actions/newsActions";
+import Slider from "react-slick";
+
+const settings = {
+  dots: false,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  swipeToSlide: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnFocus: true,
+  arrows: false,
+  lazyLoad: false,
+  centerMode: false,
+};
+
+export default function MatchLiveScroll(props) {
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const router = useRouter();
+  const { announcement } = props;
+  console.log("announcement:::", announcement);
+  const { banners } = useSelector((state) => state.banner);
+  const dispatch = useDispatch();
+  const { i18n } = useTranslation();
+  const [newsList, setNewsList] = useState([]);
+
+  return (
+    <>
+      <div style={{ paddingRight: "10px", cursor: "pointer" }}>
+        <Grid
+          sx={{
+            borderBottom: "2px solid grey",
+            marginY: "10px",
+            paddingBottom: "10px",
+            minWidth: "200px",
+          }}
+        >
+          <Typography
+            title="hshs"
+            className="twoLinesEllip"
+            sx={{
+              fontSize: "14px",
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            }}
+            px={1}
+          >
+            isjs vs jhsjf
+          </Typography>
+          <Typography sx={{ fontSize: "12px", color: "#8C8C8C" }} px={1}>
+            04/03/23
+          </Typography>
+        </Grid>
+      </div>
+    </>
+  );
+}
