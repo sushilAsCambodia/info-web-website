1cc4deaa6b867a8a495924f8f623a0d8a67f7e06
vo1an <ngetvirakvoan@gmail.com>
2023-05-24 17:21:54 +0700

    minor change verticle chart match detail

--- src/components/match/MatchVerticleChart.js
+++ src/components/match/MatchVerticleChart.js
@@ -22,54 +22,56 @@ import LinearProgress, {
 } from "@mui/material/LinearProgress";
 import { styled } from "@mui/material/styles";
 
-
-
-
+const matchInfo = [
+  {
+    id: 1,
+    title: "72`",
+    icon: <Icon icon="openmoji:soccer-ball" />,
+    playerName: "Rodrigo Moreno",
+    summary: "assist: crysencio uefueh f hsdf ih",
+  },
+  {
+    id: 2,
+    title: "65`",
+    icon: <Icon icon="fluent:arrow-sort-28-filled" />,
+    playerName: "Rodrigo Moreno",
+    summary: "assist: crysencio uefueh bsdjvbsdh",
+  },
+  {
+    id: 3,
+    title: "3`",
+    icon: "",
+    playerName: "Rodrigo Moreno",
+    summary: "assist: crysencio",
+  },
+  {
+    id: 4,
+    title: "42`",
+    icon: <Icon icon="tabler:rectangle-vertical-filled" />,
+    playerName: "Rodrigo Moreno",
+    summary: "assist: crysencio",
+  },
+];
 
 export default function MatchVerticleChart(props) {
   const { t } = useTranslation();
   const theme = useTheme();
   const router = useRouter();
 
-  return (
-  <>
-  <div class="timeline">
-  <div class="container left">
-    <div class="content">
-      <h2>2017</h2>
-      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2016</h2>
-      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-    </div>
-  </div>
-  <div class="container left">
-    <div class="content">
-      <h2>2015</h2>
-      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2012</h2>
-      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-    </div>
-  </div>
-  <div class="container left">
-    <div class="content">
-      <h2>2011</h2>
-      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2007</h2>
-      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-    </div>
-  </div>
-</div></>
-  );
+  return matchInfo.map((item, index) => {
+    return (
+      <div style={{width:'100vw'}}>
+         <div class="timeline">
+          <div style={{"--title":`'${item.title}'`}} class={`${index%2 == 0 ? "left":"right" } container`}>
+            <div class="content">
+              <h2>{item.playerName}</h2>
+              <p>
+                {item.summary}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
 }
