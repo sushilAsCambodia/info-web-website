40d6be6dd80931c2849286b2c83f1f78910a6b21
arun <arunc.loma@gmail.com>
2023-05-26 14:34:51 +0700

    update: get full

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      })
+    // ...add more providers here
+  ],
+  callbacks: {
+    async jwt({  token, account }) {
+        // Persist the OAuth access_token to the token right after signin
+        if (account) {
+            token.access_token = account.access_token;
+            console.log('account=>',account)
+            console.log('token=>',token)
+        }
+      return token
+    },
+  },
+}
+export default NextAuth(authOptions)
