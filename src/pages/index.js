32c1355c76dc24091f2daec751f82c7e4bd139fa
vo1an <ngetvirakvoan@gmail.com>
2023-06-08 14:27:41 +0700

    landing page change

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,10 +93,10 @@ const langKey = useSelector((state) => state && state.load_language && state.loa
               position: "absolute",
               left: "0",
               right: "0",
-              bottom: "150px",
+              bottom: "200px",
             }}
           >
-            <Typography fontWeight={700} fontSize="18px" lineHeight="27px"  textTransform="uppercase">
+            <Typography color="black" fontWeight={700} fontSize="18px" lineHeight="27px"  textTransform="uppercase">
               {langKey && langKey.download_app}
             </Typography>
           </Grid>
@@ -107,7 +107,7 @@ const langKey = useSelector((state) => state && state.load_language && state.loa
               position: "absolute",
               left: "0",
               right: "0",
-              bottom: "90px",
+              bottom: "140px",
             }}
           >
             <Grid item xs={12} display="flex" container spacing={2}>
@@ -131,11 +131,13 @@ const langKey = useSelector((state) => state && state.load_language && state.loa
               position: "absolute",
               left: "0",
               right: "0",
-              bottom: "35px",
+              bottom: "65px",
             }}
           >
-            <Grid item>
-              <Button href="/home" variant="outlined"style={{textTransform:"initial", borderRadius:"4px", fontSize:"14px"}}> {langKey && langKey.continue_to_home} </Button>
+            <Grid>
+              <Button href="/home" variant="outlined"style={{textTransform:"initial", borderRadius:"4px", fontSize:"14px"}}>
+                 {langKey && langKey.continue_to_home}
+                 </Button>
             </Grid>
           </Grid>
         </Grid>
