8eb5be050ae4eee33d34ed647e153f4141dd7cf4
arun <arunc.loma@gmail.com>
2023-06-09 18:02:18 +0700

    fixed: bug profile and lottery

--- src/pages/lottery.js
+++ src/pages/lottery.js
@@ -10,13 +10,17 @@ import LotteryHistoryCard from "@/components/lottery/LotteryHistoryCard";
 import NoSsr from "@mui/base/NoSsr";
 import { Icon } from "@iconify/react";
 
-import router from "next/router";
+import router, { useRouter } from "next/router";
 import MatchWithDates from "@/components/match/MatchWithDates";
 import MatchWithRounds from "@/components/match/MatchWithRounds";
 import FavouritePage from "@/components/favourite/favouritePage";
+import { getLotteryCategory,getLotteryResultByCategory } from "@/store/actions/lotteryActions";
+import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import utils from "@/common/utils";
+import DataLoading from "@/components/DataLoading";
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
-
   return (
     <div
       role="tabpanel"
@@ -45,11 +49,53 @@ function a11yProps(index) {
 
 export default function Lottery() {
   const [value, setValue] = React.useState(0);
-
+  const { i18n } = useTranslation();
+  const {loading, lotteryCategories = [], lotteryResults = []} = useSelector(state => state.lottery)
+  const router = useRouter();
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
+  const dispatch = useDispatch();
+  const onChangeTab = (hash) => {
+    router.replace(`${router.route}#${hash}`)
+  }
+  const handleGetCategory = React.useCallback(() => {
+    dispatch(
+      getLotteryCategory({
+        params: {
+          lang_id:utils.convertLangCodeToID(i18n.language)
+        }
+      })
+    ); 
+  },[dispatch,i18n.language]);
+  const handleGetLotteryResult = React.useCallback((categoryId = undefined) => {
+    dispatch(
+      getLotteryResultByCategory({
+        params: {
+          lang_id:utils.convertLangCodeToID(i18n.language),
+          category_id: categoryId
+        }
+      })
+    ); 
+  },[dispatch,i18n.language]);
+  React.useEffect(() => {
+    handleGetCategory();
+  },[handleGetCategory]);
+  React.useEffect(() => {
+    console.log(lotteryCategories,'alotteryCategories')
+  },[lotteryCategories])
+  React.useEffect(() => {
+    console.log('value',value)
+    if(value >= 0) {
+      let hash = '';
+      const lotteryCategory = lotteryCategories[value - 1] || {};
+      if(Object.keys(lotteryCategory).length) {
+        hash = '#'+(lotteryCategory?.translation?.translation);
+      }
+      // router.replace(`${router.route}${hash}`)
+        handleGetLotteryResult(lotteryCategory?.id);
+    }
+  },[value]);
   return (
     <NoSsr>
       <Box sx={{ width: "100%" }}>
@@ -71,7 +117,7 @@ export default function Lottery() {
             textTransform="capitalize"
             onChange={handleChange}
             aria-label="basic tabs example"
-         
+        
             TabIndicatorProps={{
               style: {
                 background: "#FF6F31",
@@ -87,15 +133,23 @@ export default function Lottery() {
               },
             }}
           >
-            <Tab className="lotterytab" label="Guānzhù" {...a11yProps(0)} />
-            <Tab className="lotterytab" label="History" {...a11yProps(1)} />
-            <Tab className="lotterytab" label="Soccer" {...a11yProps(2)} />
-            <Tab className="lotterytab" label="Basket Ball" {...a11yProps(3)} />
-            <Tab className="lotterytab" label="xxx Cǎixxx" {...a11yProps(4)} />
-            <Tab className="lotterytab" label="Favourite" {...a11yProps(5)} />
+            <Tab  className="lotterytab" label="all" {...a11yProps(0)}/>
+            {
+              lotteryCategories.map((lc,index) => {
+                return (
+                  <Tab key={index} className="lotterytab" label={lc?.translation?.translation} {...a11yProps((index+1))}/>
+                )
+              })
+            }
+            {/* <Tab className="lotterytab" label="Guānzhù" {...a11yProps(0)}/>
+            <Tab className="lotterytab" label="History" {...a11yProps(1)}/>
+            <Tab className="lotterytab" label="Soccer" {...a11yProps(2)}/>
+            <Tab className="lotterytab" label="Basket Ball" {...a11yProps(3)}/>
+            <Tab className="lotterytab" label="xxx Cǎixxx" {...a11yProps(4)}/>
+            <Tab className="lotterytab" label="Favourite" {...a11yProps(5)}/> */}
           </Tabs>
           {value == 1 ? '':
-         <> <Button
+        <> <Button
             variant="contained"
             sx={{
               background: "linear-gradient(90deg, #FF0000 0%, #FF6E31 100%)",
@@ -106,10 +160,10 @@ export default function Lottery() {
               height: 30,
               width: 58,
               zIndex: 9,
-             
+            
               transform: "translate(0, -38%)",
             }}
-           
+          
           >
             <Typography
               component="span"
@@ -165,110 +219,70 @@ export default function Lottery() {
               width: "100%",
             }}
           >
-            <LotteryCard />
-            <Box height={12}></Box>
-            <LotteryCard /> 
-            <Box height={12}></Box>
-            <LotteryCard />
-            <Box height={12}></Box>
-            <LotteryCard />
-            <Box height={12}></Box>
-            <LotteryCard />
-            <Box height={12}></Box>
-            <LotteryCard />
+            {
+              loading ? <DataLoading/> : (
+                lotteryResults.map((lr,key) => {
+                  return (
+                    <div key={key}>
+                      <LotteryCard lottery={lr}/>
+                      <Box height={12}></Box> 
+                    </div>
+                  );
+                })
+              )
+            }
           </Grid>
           </Grid>
-        
         </TabPanel>
-        <TabPanel value={value} index={1}>
-          <Grid
-            item
-            xs={12}
-            container
-            justifyContent="space-between"
-            alignItems="center"
-            style={{ marginTop: "50px", padding: 10 }}
-          >
-            <Typography>Super Loto 2390</Typography>
-            <Grid >
-              {/* <Button
-               size="small"
-               textTransform="capitalize !important"
-                variant="outlined"
-                sx={{ border: "1px solid #ddd",  color: "#8C8C8C", textTransform:"capitalize !important" }}
-                onClick={()=>{router.push('/games')}}
-              >
-                <Icon width={15} icon="icon-park-solid:game-ps" />
-                <Typography fontSize="12px">Games</Typography>
-              </Button> */}
-
-              <Button
-             size="small"
-             textTransform="capitalize !important"
-              variant="outlined"
-              sx={{ border: "1px solid #ddd",  color: "#8C8C8C", textTransform:"capitalize !important" }}
-              onClick={()=>{router.push('/games')}}
-           
-          >
-            <Typography
-              component="span"
-              sx={{
-                whiteSpace: "nowrap",
-                fontSize: "11px",
-                display: "flex",
-                alignItems: "center",
-                textTransform: "capitalize",
-              }}
-            >
-              <svg
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-               <path d="M5.5 0.25H1C0.801088 0.25 0.610322 0.329018 0.46967 0.46967C0.329018 0.610322 0.25 0.801088 0.25 1V5.5C0.25 5.69891 0.329018 5.88968 0.46967 6.03033C0.610322 6.17098 0.801088 6.25 1 6.25H5.5C5.69891 6.25 5.88968 6.17098 6.03033 6.03033C6.17098 5.88968 6.25 5.69891 6.25 5.5V1C6.25 0.801088 6.17098 0.610322 6.03033 0.46967C5.88968 0.329018 5.69891 0.25 5.5 0.25ZM13 7.75H8.5C8.30109 7.75 8.11032 7.82902 7.96967 7.96967C7.82902 8.11032 7.75 8.30109 7.75 8.5V13C7.75 13.1989 7.82902 13.3897 7.96967 13.5303C8.11032 13.671 8.30109 13.75 8.5 13.75H13C13.1989 13.75 13.3897 13.671 13.5303 13.5303C13.671 13.3897 13.75 13.1989 13.75 13V8.5C13.75 8.30109 13.671 8.11032 13.5303 7.96967C13.3897 7.82902 13.1989 7.75 13 7.75ZM10.75 0.25C9.0955 0.25 7.75 1.5955 7.75 3.25C7.75 4.9045 9.0955 6.25 10.75 6.25C12.4045 6.25 13.75 4.9045 13.75 3.25C13.75 1.5955 12.4045 0.25 10.75 0.25ZM3.25 7.75C1.5955 7.75 0.25 9.0955 0.25 10.75C0.25 12.4045 1.5955 13.75 3.25 13.75C4.9045 13.75 6.25 12.4045 6.25 10.75C6.25 9.0955 4.9045 7.75 3.25 7.75Z" fill="#8C8C8C"/>
-              </svg>
-              &nbsp; Games
-            </Typography>
-          </Button>
-
-
-            </Grid>
-          </Grid>
-          <Grid item xs={12} style={{ padding: 10 }}>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-            <Box height={12}></Box>
-            <LotteryHistoryCard />
-          </Grid>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <MatchWithDates />  
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-        <MatchWithRounds />
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          Shíshí Cǎi
-        </TabPanel>
-        <TabPanel value={value} index={5}>
-        <FavouritePage />
-
-        </TabPanel>
+        {
+          lotteryCategories.map((lc,index) => {
+            return (
+              <TabPanel key={index} value={value} index={(index+1)}>
+                <Grid
+                  item
+                  xs={12}
+                  style={{
+                    backgroundImage: "url('/assets/stadium.png')",
+                    height: 150,
+                    position: "relative",
+                  }}
+                >
+                  <span
+                    style={{
+                      background: "rgba(0, 0, 0, 0.7)",
+                      position: "absolute",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  ></span>
+                    <Grid
+                  item
+                  xs={12}
+                  style={{
+                    padding: 10,
+                    position: "absolute",
+                    top: 50,
+                    width: "100%",
+                  }}
+                >
+                  {
+                    loading ? <DataLoading/> : (
+                      lotteryResults.map((lr,key) => {
+                        return (
+                          <div key={key}>
+                            <LotteryCard lottery={lr}/>
+                            <Box height={12}></Box> 
+                          </div>
+                        );
+                      })
+                    )
+                  }
+                </Grid>
+                </Grid>
+              </TabPanel>
+            )
+          })
+        }
       </Box>
     </NoSsr>
   );
